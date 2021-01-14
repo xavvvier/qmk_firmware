45cc0bccf759a4caf40eb044c6fa4a096221046d
xavvvier <xavvvier@users.noreply.github.com>
2021-01-14 08:40:47 -0400

    swap enter and backspace

--- keyboards/lily58/keymaps/xavvvier/keymap.c
+++ keyboards/lily58/keymaps/xavvvier/keymap.c
@@ -1,3 +1,9 @@
+/* 
+compile:
+qmk compile -kb lily58 -km xavvvier
+flash:
+qmk flash -kb lily58 -km xavvvier
+*/
 #include QMK_KEYBOARD_H
 
 #ifdef PROTOCOL_LUFA
@@ -24,23 +30,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  '   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Enter|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | BKSP |
+ * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | '    |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCtr | LAlt |  LGUI| / Enter /       \Space \  |   =  |LOWER | RAISE |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | LCtr | LAlt |  LGUI| / BKSP  /       \Space \  |   =  |LOWER | RAISE |
+ *                   |      |      |      |/       /         \      \ |      |      |       |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT( \
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSPC, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT, \
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LCTL, KC_LALT, KC_LGUI, KC_ENT, KC_SPC, KC_EQL , MO(_LOWER), MO(_RAISE) \
+                        KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, KC_SPC, KC_EQL , MO(_LOWER), MO(_RAISE) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -70,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |CapsLo|      |  Up  |      |      |      |                    | V.Up |      |      |      |   |  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | Left | Down |Right |      |      |-------.    ,-------|V.Dwn |      |      |      |  Up  | Del  |
+ * |      | Left | Down |Right |      |      |-------.    ,-------|V.Dwn |      |      |      |  Up  |      |
  * |------+------+------+------+------+------| Home  |    |  End  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|V.Mute|      |      | Left | Down |Right |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      | /  Del  /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -82,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_BSLS, KC_F1  ,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,  KC_F8,  KC_F9, KC_F10,   KC_F11, \
   KC_CAPS, _______,   KC_UP, _______, _______, _______,                     KC__VOLUP, _______,_______,_______,KC_PIPE,   KC_F12, \
-  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   KC__VOLDOWN, _______,_______,_______, KC_UP ,   KC_DEL, \
+  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   KC__VOLDOWN, _______,_______,_______, KC_UP ,  _______, \
   _______, _______, _______, _______, _______, _______, KC_HOME , KC_END ,   KC__MUTE, _______,_______,KC_LEFT,KC_DOWN,  KC_RGHT, \
-                             _______, _______, _______,  _______, _______,    _______, _______, _______ \
+                             _______, _______, _______,  KC_DEL , _______,    _______, _______, _______ \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
